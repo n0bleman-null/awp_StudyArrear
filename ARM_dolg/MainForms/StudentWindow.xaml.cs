77c01e2f4b2fdfa111a245c9660a44335bb367cd
Тимофей Крук <war18@tut.by>
2021-06-07 15:35:41 +0300

    ready

--- ARM_dolg/MainForms/StudentWindow.xaml.cs
+++ ARM_dolg/MainForms/StudentWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Reflection;
 using Word = Microsoft.Office.Interop.Word;
+using Microsoft.EntityFrameworkCore;
 
 namespace ARM_dolg
 {
@@ -46,10 +47,41 @@ namespace ARM_dolg
                                      }).ToList();
 
             }
-            //Sum.Text += Dolgi.Items.Count * 18;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Dolgi.SelectedItem is null || Dolgi.SelectedItems.Count != 1)
+            {
+                MessageBox.Show("Для печати заявление выберите одно занятие для отработки");
+                return;
+            }
+            object oMissing = System.Reflection.Missing.Value;
+
+            Word._Application oWord;
+            Word._Document oDoc;
+            oWord = new Word.Application();
+            oWord.Visible = true;
+            
+            using (var dc = new DolgContext())
+            {
+                var lab = Dolgi.SelectedItem;
+                object oTemplate = @"D:\ARM_dolg\Отработка.dotx";
+                oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
+                object oName = @"Фио";
+                oDoc.Bookmarks[ref oName].Range.Text = Student.Фио;
+                object oGroup = @"Группа";
+                oDoc.Bookmarks[ref oGroup].Range.Text = dc.StudGroups.Single(g => g.Id == Student.НомерГруппы).Номер;
+                object oToday = @"Сегодня";
+                oDoc.Bookmarks[ref oToday].Range.Text = DateTime.Today.ToShortDateString();
+                object oDate = @"Дата";
+                oDoc.Bookmarks[ref oDate].Range.Text = ((DateTime)lab.GetType().GetProperty("ДатаЗанятия").GetValue(lab)).ToShortDateString();
+                object oSubj = @"Предмет";
+                oDoc.Bookmarks[ref oSubj].Range.Text = (string)lab.GetType().GetProperty("Предмет").GetValue(lab);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             object oMissing = System.Reflection.Missing.Value;
 
@@ -57,16 +89,17 @@ namespace ARM_dolg
             Word._Document oDoc;
             oWord = new Word.Application();
             oWord.Visible = true;
-
-            object oTemplate = @"D:\ARM_dolg\отчисление.dotx";
-            oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
-            object oBookmark = @"Студент";
-            oDoc.Bookmarks[ref oBookmark].Range.Text = Student.Фио;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
+            using (var dc = new DolgContext())
+            {
+                object oTemplate = @"D:\ARM_dolg\Отчисление.dotx";
+                oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
+                object oName = @"Фио";
+                oDoc.Bookmarks[ref oName].Range.Text = Student.Фио;
+                object oGroup = @"Группа";
+                oDoc.Bookmarks[ref oGroup].Range.Text = dc.StudGroups.Single(g => g.Id == Student.НомерГруппы).Номер;
+                object oToday = @"Сегодня";
+                oDoc.Bookmarks[ref oToday].Range.Text = DateTime.Today.ToShortDateString();
+            }
         }
     }
 }
